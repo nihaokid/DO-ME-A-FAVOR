df2f002409b1084f95652ec51d71fd268c55f02e
nihaokid <zhaiwei_xjtu@163.com>
2013-06-28 23:43:35 +0800

    init do me a favor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoMeAFavor::Application.config.secret_key_base = '8b6d2af0000aaad655be9bc770dc44e586273c46356280b874915459fa224737397f1b7605b36088aa6930a87b3667f63fd1e73c5f991b1a5a8cbce8ae03b4e6'
